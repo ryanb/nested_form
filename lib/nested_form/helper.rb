0d02ec7986d6b8a4db064fd9c571e36588c750ce
Ryan Bates <ryan@railscasts.com>
2010-01-11 14:20:26 -0800

    adding nested_form_for method which adds the :builder => NestedForm::Builder option

--- lib/nested_form/helper.rb
+++ lib/nested_form/helper.rb
@@ -1,5 +1,8 @@
 module NestedForm
   module Helper
-    
+    def nested_form_for(*args, &block)
+      options = args.extract_options!.reverse_merge(:builder => NestedForm::Builder)
+      form_for(*(args << options), &block)
+    end
   end
 end
--- spec/nested_form/helper_spec.rb
+++ spec/nested_form/helper_spec.rb
@@ -1,5 +1,13 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe NestedForm::Helper do
+  before(:each) do
+    @helper = Object.new
+    @helper.extend NestedForm::Helper
+  end
   
+  it "should pass nested form builder to form_for along with other options" do
+    mock(@helper).form_for(:first, :second, :other => :arg, :builder => NestedForm::Builder)
+    @helper.nested_form_for(:first, :second, :other => :arg)
+  end
 end
