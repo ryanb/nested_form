0ac5f0b88c9a7389e709d92dce9f9591118c4d96
Ryan Bates <ryan@railscasts.com>
2010-01-11 23:35:38 -0800

    apply all callbacks for content after nested form

--- lib/nested_form/view_helper.rb
+++ lib/nested_form/view_helper.rb
@@ -3,11 +3,15 @@ module NestedForm
     def nested_form_for(*args, &block)
       options = args.extract_options!.reverse_merge(:builder => NestedForm::Builder)
       form_for(*(args << options), &block) 
-      @after_nested_form_callback.call if @after_nested_form_callback
+      @after_nested_form_callbacks ||= []
+      @after_nested_form_callbacks.each do |callback|
+        callback.call
+      end
     end
     
     def after_nested_form(&block)
-      @after_nested_form_callback = block
+      @after_nested_form_callbacks ||= []
+      @after_nested_form_callbacks << block
     end
   end
 end
--- spec/nested_form/view_helper_spec.rb
+++ spec/nested_form/view_helper_spec.rb
@@ -2,25 +2,27 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe NestedForm::ViewHelper do
   before(:each) do
-    @helper = ActionView::Base.new
-    @helper.output_buffer = ""
-    stub(@helper).url_for { "" }
-    stub(@helper).protect_against_forgery? { false }
+    @template = ActionView::Base.new
+    @template.output_buffer = ""
+    stub(@template).url_for { "" }
+    stub(@template).protect_against_forgery? { false }
   end
   
   it "should pass nested form builder to form_for along with other options" do
-    mock(@helper).form_for(:first, :second, :other => :arg, :builder => NestedForm::Builder)
-    @helper.nested_form_for(:first, :second, :other => :arg)
+    mock(@template).form_for(:first, :second, :other => :arg, :builder => NestedForm::Builder)
+    @template.nested_form_for(:first, :second, :other => :arg)
   end
   
   it "should pass instance of NestedForm::Builder to nested_form_for block" do
-    @helper.nested_form_for(:project, Project) do |f|
+    @template.nested_form_for(:project, Project) do |f|
       f.should be_instance_of(NestedForm::Builder)
     end
   end
   
   it "should append content to end of nested form" do
-    @helper.after_nested_form { "after_nested_form" }
-    @helper.nested_form_for(:project, Project) {}.should include("after_nested_form")
+    @template.after_nested_form { @template.concat("123") }
+    @template.after_nested_form { @template.concat("456") }
+    @template.nested_form_for(:project, Project) {}
+    @template.output_buffer.should include("123456")
   end
 end
