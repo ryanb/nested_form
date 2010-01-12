c9bf82313d041c9bb59caa286182abafa7a41589
Ryan Bates <ryan@railscasts.com>
2010-01-11 16:57:11 -0800

    adding callback for appending content to the end of the nested form

--- lib/nested_form/view_helper.rb
+++ lib/nested_form/view_helper.rb
@@ -2,7 +2,12 @@ module NestedForm
   module ViewHelper
     def nested_form_for(*args, &block)
       options = args.extract_options!.reverse_merge(:builder => NestedForm::Builder)
-      form_for(*(args << options), &block)
+      form_for(*(args << options), &block) 
+      concat(@after_nested_form_callback.call) if @after_nested_form_callback
+    end
+    
+    def after_nested_form(&block)
+      @after_nested_form_callback = block
     end
   end
 end
