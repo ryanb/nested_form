f0773577cd242644a21d9dc5591117fc0f5c5621
andrea singh <info@madebydna.com>
2010-10-17 16:13:15 -1000

    added source root to generator file

--- lib/generators/nested_form/install_generator.rb
+++ lib/generators/nested_form/install_generator.rb
@@ -1,6 +1,11 @@
 module NestedForm
   module Generators
     class InstallGenerator < Rails::Generators::Base
+      def self.source_root
+        File.dirname(__FILE__) + "/templates"
+      end
+      
+      
       def copy_jquery_file
         copy_file 'nested_form.js', 'public/javascripts/nested_form.js'
       end
