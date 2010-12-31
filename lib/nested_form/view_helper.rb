02c573833671e482efdd262a049446e8e9c39aae
andrea singh <info@madebydna.com>
2010-12-30 16:11:49 -1000

    fixed (hacked) error: can't add a new key into hash during iteration

--- lib/nested_form/view_helper.rb
+++ lib/nested_form/view_helper.rb
@@ -3,16 +3,20 @@ module NestedForm
     def nested_form_for(*args, &block)
       options = args.extract_options!.reverse_merge(:builder => NestedForm::Builder)
       output = form_for(*(args << options), &block)
-      @after_nested_form_callbacks ||= {}
-      fields = @after_nested_form_callbacks.map do |key, callback|
+      @after_nested_form_callbacks ||= []
+      fields = @after_nested_form_callbacks.map do |callback|
         callback.call
       end
       output << fields.join(" ").html_safe
     end
     
     def after_nested_form(association, &block)
-      @after_nested_form_callbacks ||= {}
-      @after_nested_form_callbacks[association] = block
+      @associations ||= []
+      @after_nested_form_callbacks ||= []
+      unless @associations.include?(association)
+        @associations << association 
+        @after_nested_form_callbacks << block
+      end
     end
   end
 end
