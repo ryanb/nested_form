013a558be64329a915809003ae8b4a401bbd3e2a
Peter Giacomo Lombardo <pglombardo@ourstage.com>
2011-02-12 18:32:56 -0500

    Rename template files to their singular form. Improved test of javascript framework with File.exists?()

--- lib/generators/nested_form/install_generator.rb
+++ lib/generators/nested_form/install_generator.rb
@@ -5,10 +5,8 @@ module NestedForm
         File.dirname(__FILE__) + "/templates"
       end
       
-      
       def copy_jquery_file
-        stat = File.stat('public/javascripts/prototype.js')
-        if stat.file?
+        if File.exists?('public/javascripts/prototype.js')
           copy_file 'prototype_nested_form.js', 'public/javascripts/nested_form.js'
         else
           copy_file 'jquery_nested_form.js', 'public/javascripts/nested_form.js'
