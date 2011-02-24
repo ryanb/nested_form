6d885ed3aa2c894e5da077cfaa118e584bf7c6d4
Ellis Berner <eberner@gmail.com>
2011-02-24 13:25:56 -0800

    added iterations for link_to_add

--- lib/nested_form/builder.rb
+++ lib/nested_form/builder.rb
@@ -1,15 +1,17 @@
 module NestedForm
   class Builder < ::ActionView::Helpers::FormBuilder
-    def link_to_add(name, association)
-      @fields ||= {}
-      @template.after_nested_form(association) do
-        model_object = object.class.reflect_on_association(association).klass.new
-        output = %Q[<div id="#{association}_fields_blueprint" style="display: none">].html_safe
-        output << fields_for(association, model_object, :child_index => "new_#{association}", &@fields[association])
-        output.safe_concat('</div>')
-        output
+    def link_to_add(name, association, iterations = 1)
+      iterations.times do
+        @fields ||= {}
+        @template.after_nested_form(association) do
+          model_object = object.class.reflect_on_association(association).klass.new
+          output = %Q[<div id="#{association}_fields_blueprint" style="display: none">].html_safe
+          output << fields_for(association, model_object, :child_index => "new_#{association}", &@fields[association])
+          output.safe_concat('</div>')
+          output
+        end
+        @template.link_to(name, "javascript:void(0)", :class => "add_nested_fields", "data-association" => association)
       end
-      @template.link_to(name, "javascript:void(0)", :class => "add_nested_fields", "data-association" => association)
     end
 
     def link_to_remove(name)
