8bb1118345b7c6652fdd3689ab0522fa523f3efb
Clemens Kofler <clemens@railway.at>
2012-07-26 18:06:11 +0200

    Fix: Actually, Element.getOffsetParent is needed

--- vendor/assets/javascripts/prototype_nested_form.js
+++ vendor/assets/javascripts/prototype_nested_form.js
@@ -6,7 +6,7 @@ document.observe('click', function(e, el) {
 
 	  // Make the context correct by replacing new_<parents> with the generated ID
 	  // of each of the parent objects
-	  var context = (el.up('.fields').firstDescendant().readAttribute('name') || '').replace(new RegExp('\[[a-z]+\]$'), '');
+	  var context = (el.getOffsetParent('.fields').firstDescendant().readAttribute('name') || '').replace(new RegExp('\[[a-z]+\]$'), '');
 
 	  // context will be something like this for a brand new form:
 	  // project[tasks_attributes][new_1255929127459][assignments_attributes][new_1255929128105]
