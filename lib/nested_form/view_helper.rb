c74ebac9b013d678cd3b9ab88502f7457ddb22f3
Sergey Nartimov <just.lest@gmail.com>
2012-11-10 18:21:35 +0300

    use capture helper instead of yield

--- lib/nested_form/view_helper.rb
+++ lib/nested_form/view_helper.rb
@@ -2,36 +2,36 @@ require 'nested_form/builders'
 
 module NestedForm
   module ViewHelper
-    def nested_form_for(*args)
+    def nested_form_for(*args, &block)
       options = args.extract_options!.reverse_merge(:builder => NestedForm::Builder)
       form_for(*(args << options)) do |f|
-        yield(f).to_s << after_nested_form_callbacks
+        capture(f, &block).to_s << after_nested_form_callbacks
       end
     end
 
     if defined?(NestedForm::SimpleBuilder)
-      def simple_nested_form_for(*args)
+      def simple_nested_form_for(*args, &block)
         options = args.extract_options!.reverse_merge(:builder => NestedForm::SimpleBuilder)
         simple_form_for(*(args << options)) do |f|
-          yield(f).to_s << after_nested_form_callbacks
+          capture(f, &block).to_s << after_nested_form_callbacks
         end
       end
     end
 
     if defined?(NestedForm::FormtasticBuilder)
-      def semantic_nested_form_for(*args)
+      def semantic_nested_form_for(*args, &block)
         options = args.extract_options!.reverse_merge(:builder => NestedForm::FormtasticBuilder)
         semantic_form_for(*(args << options)) do |f|
-          yield(f).to_s << after_nested_form_callbacks
+          capture(f, &block).to_s << after_nested_form_callbacks
         end
       end
     end
 
     if defined?(NestedForm::FormtasticBootstrapBuilder)
-      def semantic_bootstrap_nested_form_for(*args)
+      def semantic_bootstrap_nested_form_for(*args, &block)
         options = args.extract_options!.reverse_merge(:builder => NestedForm::FormtasticBootstrapBuilder)
         semantic_form_for(*(args << options)) do |f|
-          yield(f).to_s << after_nested_form_callbacks
+          capture(f, &block).to_s << after_nested_form_callbacks
         end
       end
     end
