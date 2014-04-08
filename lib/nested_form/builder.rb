1310d7978a91e8bd36b0ffc0edf8e6404faee61e
Dale Stevens <dale@betterfinance.me>
2014-04-08 11:47:34 -0700

    • Improved Installation • Works more modernly to the master branch

--- lib/nested_form/builder.rb
+++ lib/nested_form/builder.rb
@@ -2,7 +2,7 @@ module NestedForm
   class Builder < ActionView::Helpers::FormBuilder
     def link_to_add(name, association)
       @fields ||= {}
-      @template.after_nested_form do
+      @template.after_nested_form(association) do
         model_object = object.class.reflect_on_association(association).klass.new
         @template.concat(%Q[<div id="#{association}_fields_blueprint" style="display: none">])
         fields_for(association, model_object, :child_index => "new_#{association}", &@fields[association])
@@ -10,22 +10,29 @@ module NestedForm
       end
       @template.link_to(name, "javascript:void(0)", :class => "add_nested_fields", "data-association" => association)
     end
-    
+
     def link_to_remove(name)
       hidden_field(:_destroy) + @template.link_to(name, "javascript:void(0)", :class => "remove_nested_fields")
     end
-    
+
     def fields_for_with_nested_attributes(association, args, block)
       @fields ||= {}
       @fields[association] = block
       super
     end
 
-    
-    def fields_for_nested_model(name, association, args, block)
-      @template.concat('<div class="fields">')
-      super
-      @template.concat('</div>')
+    def fields_for_nested_model(name, object, args, block)
+      @template.concat "<div class='#{object.class.name.underscore}_fields fields'>"
+      if object.new_record?
+        @template.fields_for(name, object, *args, &block)
+      else
+        @template.fields_for(name, object, *args) do |builder|
+          block.call(builder)
+          @template.concat builder.hidden_field(:id) unless builder.emitted_hidden_id?
+        end
+      end
+      @template.concat '</div>'
     end
+
   end
 end
--- lib/nested_form/view_helper.rb
+++ lib/nested_form/view_helper.rb
@@ -1,18 +1,59 @@
+require 'nested_form/builder'
+
 module NestedForm
   module ViewHelper
     def nested_form_for(*args, &block)
       options = args.extract_options!.reverse_merge(:builder => NestedForm::Builder)
-      form_for(*(args << options), &block) 
-      @after_nested_form_callbacks ||= []
-      @after_nested_form_callbacks.each do |callback|
-        callback.call
+      form_for(*(args << options)) do |f|
+        block.call(f).to_s << after_nested_form_callbacks
       end
     end
-    
-    def after_nested_form(&block)
-      @after_nested_form_callbacks ||= []
-      @after_nested_form_callbacks << block
+
+    if defined?(NestedForm::SimpleBuilder)
+      def simple_nested_form_for(*args, &block)
+        options = args.extract_options!.reverse_merge(:builder => NestedForm::SimpleBuilder)
+        simple_form_for(*(args << options)) do |f|
+          block.call(f).to_s << after_nested_form_callbacks
+        end
+      end
     end
+
+    if defined?(NestedForm::FormtasticBuilder)
+      def semantic_nested_form_for(*args, &block)
+        options = args.extract_options!.reverse_merge(:builder => NestedForm::FormtasticBuilder)
+        semantic_form_for(*(args << options)) do |f|
+          block.call(f).to_s << after_nested_form_callbacks
+        end
+      end
+    end
+
+    if defined?(NestedForm::FormtasticBootstrapBuilder)
+      def semantic_bootstrap_nested_form_for(*args, &block)
+        options = args.extract_options!.reverse_merge(:builder => NestedForm::FormtasticBootstrapBuilder)
+        semantic_form_for(*(args << options)) do |f|
+          block.call(f).to_s << after_nested_form_callbacks
+        end
+      end
+    end
+
+    def after_nested_form(association, &block)
+      @associations ||= []
+      @after_nested_form_callbacks ||= []
+      unless @associations.include?(association)
+        @associations << association
+        @after_nested_form_callbacks << block
+      end
+    end
+
+    private
+      def after_nested_form_callbacks
+        @after_nested_form_callbacks ||= []
+        fields = []
+        while callback = @after_nested_form_callbacks.shift
+          fields << callback.call
+        end
+        fields.join(" ").html_safe
+      end
   end
 end
 
