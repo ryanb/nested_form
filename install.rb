f69f978fae58a983c4047f428076ccc3a232f583
Dale Stevens <dale@betterfinance.me>
2014-05-19 15:29:29 -0700

    should fix the install issue

--- install.rb
+++ install.rb
@@ -1,7 +1,10 @@
 copy_files = [
-  {:from => "vendor/plugins/nested_form/vendor/assets/javascripts/*", :to => "app/public/javascripts/"}
+  {
+    :from => "vendor/plugins/nested_form/vendor/assets/javascripts/nested_form.js",
+    :to => "#{Rails.root}/app/public/javascripts/"
+  }
 ]
 
 copy_files.each do |file|
-  FileUtils.cp("#{Rails.root}/#{file[:from]}", "#{Rails.root}/#{file[:to]}")
+  FileUtils.cp(file[:from], file[:to])
 end
